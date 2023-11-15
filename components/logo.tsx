e63fb17df5d62c223fc310e3f7f0cd1ed0ea4f45
dandamudi2 <srinivasd2mail@gmail.com>
2023-11-15 22:37:37 +0800

    first commit

---
+++ components/logo.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import localFont from 'next/font/local';
+
+import ImgLogo from '@/public/logo.svg';
+
+import { cn } from '@/lib/utils';
+const headingFont = localFont({
+  src: '../public/fonts/font.woff2',
+});
+
+export const Logo = () => {
+  return (
+    <div>
+      <Link href="/">
+        <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+          <Image src={ImgLogo} alt="Logo" height={30} width={30} />
+          <p
+            className={cn(
+              'text-lg text-neutral-700 pb-1',
+              headingFont.className
+            )}
+          >
+            Taskify
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
+};
