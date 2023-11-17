7e20f5b4d01ab3d6db6ccb8346c43c62f5e938db
dandamudi2 <srinivasd2mail@gmail.com>
2023-11-17 13:43:52 +0800

    form-delete changes

--- app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
@@ -8,10 +8,10 @@ interface BoardProps {
 }
 
 export const Board = ({ title, id }: BoardProps) => {
-  const deleteBoardWithId = deleteBoard.bind(null, id);
+  //const deleteBoardWithId = deleteBoard.bind(null, id);
 
   return (
-    <form action={deleteBoardWithId} className="flex items-center gap-x-2">
+    <form className="flex items-center gap-x-2">
       <p>Board title: {title}</p>
       <FormDelete />
     </form>
